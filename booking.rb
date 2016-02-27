e5470987e12aae8b23142e787effb21973556478
Borxburn-One <broxburn001@yahoo.co.uk>
2016-02-27 13:11:56 +0000

    get booked room no

--- booking.rb
+++ booking.rb
@@ -1,16 +1,21 @@
 class Booking
   attr_reader :booking
-  def initialize(booking)
-    @booking = booking   # an array of guests
+  def initialize(params)
+    @guests = params[:guests]   # an array of guests
+    @room_no = params[:room_no]
   end
 
   def get_booked_in_guests()
     #  pass in a booking and it returns the guests therein (single and double)
     guest_names = []
-    @booking.each do |guest|
+    @guests.each do |guest|
       guest_names << guest.get_guest_name
     end
     return guest_names
+  end
+
+  def get_booked_room_no
+    return @room_no
   end
 
 end
--- specs/booking_spec.rb
+++ specs/booking_spec.rb
@@ -14,11 +14,23 @@ class BookingTest < MiniTest::Test
     @guest_3 = Guest.new({name: 'Curly', contact: '0121-222-2332'})
     @guest_4 = Guest.new({name: 'Moe', contact: '01223-222-2332'})
 
-    @booking_1 = Booking.new([@guest_1, @guest_2])
+    @booking_1 = Booking.new({guests: [@guest_1, @guest_2], room_no: 6})
   end
 
   def test_booked_in_guests
     assert_equal(['Fred Mertz', 'Ethel Mertz'], @booking_1.get_booked_in_guests)
+  end
+
+  def test_booked_room_no
+    assert_equal(6, @booking_1.get_booked_room_no)
+  end
+
+  def test_get_checkin_date
+
+  end
+
+  def test_get_checkout_date
+
   end
 
 end 
